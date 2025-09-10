2ccb214df44bfcc59995524c538c8e043c1fde39
ArthurQuintino <arthurquintino2304@gmail.com>
2025-09-10 08:34:58 -0300

    Versionando o carrinho com funcionalidade completa

--- ProjetoLoja/Repositorio/ProdutoRepositorio.cs
+++ ProjetoLoja/Repositorio/ProdutoRepositorio.cs
@@ -6,20 +6,29 @@ namespace ProjetoLoja.Repositorio
 {
     public class ProdutoRepositorio
     {
-
         private readonly string _connectionString;
-
-        //construtor da string de conexão
 
         public ProdutoRepositorio(string connectionString)
         {
             _connectionString = connectionString;
         }
+
         public async Task<IEnumerable<Produto>> TodosProdutos()
         {
             using var connection = new MySqlConnection(_connectionString);
-            var sql = "SELECT Id, Nome, Descricao, Preco, ImageUrl, Estoque FROM Produto";
+            var sql = "SELECT Id, Nome, Descricao, Preco, ImageUrl, Estoque FROM produto";
             return await connection.QueryAsync<Produto>(sql);
         }
+
+
+        public async Task<Produto?> ProdutosPorId(int id)
+        {
+            using var connection = new MySqlConnection(_connectionString);
+            var sql = "SELECT Id, Nome, Descricao, Preco, ImageUrl, Estoque FROM produto WHERE Id = @Id";
+            return await connection.QueryFirstOrDefaultAsync<Produto>(sql, new { Id = id });
+        }
+
+
     }
+
 }
